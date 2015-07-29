deca55bee458f5af12e1e872ebe7968f42e52493
BrendonPierson <brendonpierson@gmail.com>
2015-07-29 15:23:46 -0500

    fixed jquery

--- scripts/app.js
+++ scripts/app.js
@@ -4,19 +4,28 @@ $(document).ready(function(){
   $("#submit").click(function(){
     //get user input, turn it into an array, and lowercase
     var $arrOfUserInput = $("#user-input").val().toLowerCase().split(" ");
+    console.log($arrOfUserInput);
     //get selection from select menu
     var $langSelected = $("#lang :selected").text();
-    var html = "";
+    var html = "<div>";
 
     //logic to run the correct function
-    if ($langSelected === "French") {
-        html = Translation.getFrench($arrOfUserInput);
+    if ($langSelected === "French") {   
+    }
     } else if ($langSelected === "Italian") {
         html = Translation.getItalian($arrOfUserInput);
     } else if ($langSelected === "Spanish") {
-        html = Translation.getSpanish($arrOfUserInput);
+      var returnArr = [];
+      for (var i = 0; i < $arrOfUserInput.length; i++) {
+        returnArr[returnArr.length] = Translation.getSpanish($arrOfUserInput[i]);
+      }
+      html = returnArr.join(" ");
+      html += "</div>";
     }
-
     $("#user-output").html(html);
   });
 });
+
+function translate(lang){
+
+}
