90bf57de524e049c57a9e5c5d388548de0644aee
TGriffey <tgriffey9389@gmail.com>
2015-07-30 09:20:47 -0500

    Fixed bugs in HTML and JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,21 @@
 // ** THIS IS THE MAIN IIFE -- all Translation modules will augment off this one
 
-    var Translation = (function () {
-        var spanish = {
-            "merry": "feliz",
-            "christmas": "navidad",
-            "and": "y",
-            "happy": "prospero",
-            "new": "nuevo",
-            "year": "ano"
-            }
-
-        return {
-            getSpanish: function(userInput) {
-                return spanish[userInput]
-            }
+var Translation = (function () {
+    var spanish = {
+        "merry": "feliz",
+        "christmas": "navidad",
+        "and": "y",
+        "happy": "prospero",
+        "new": "nuevo",
+        "year": "ano"
         }
 
-    })();
+    return {
+        getSpanish: function(userInput) {
+            return spanish[userInput]
+        }
+    }
+
+})();
 
 
